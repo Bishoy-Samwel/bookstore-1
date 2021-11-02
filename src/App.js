bd8c4b52f6c4106e6b5d7831ad2c6121ea83186f
Bishoy <bishoy.samwuel@gmail.com>
2021-11-02 12:16:42 +0200

    Add container to App file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+
+import Container from './components/Container';
 
 function App() {
   return (
     <div className="App">
-    
+      <Container />
     </div>
   );
 }
