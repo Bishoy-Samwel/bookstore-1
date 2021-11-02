5ff9696829fa959c5e7b93078b9f5d80e525c9d7
Bishoy <bishoy.samwuel@gmail.com>
2021-11-02 16:09:08 +0200

    Select the books from the store using useSelector

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,9 @@
 import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
 import Book from './Book';
 
 export default function Books() {
-  const book1 = {
-    name: 'Democracy The God That Failed',
-    category: 'Politics',
-    author: 'Hans-Hermann Hoppe',
-  };
-  const books = [book1];
+  const books = useSelector(state => state.books, shallowEqual);
   return (
     <ul>
       {
