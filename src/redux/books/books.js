8567d23b70bacdc7665840185a61ffe1c9212ff5
Bishoy <bishoy.samwuel@gmail.com>
2021-11-03 11:01:36 +0200

    Change the order

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,9 +1,9 @@
-const initialState = [];
 // Actions Types
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 // Reducer
+const initialState = [];
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
