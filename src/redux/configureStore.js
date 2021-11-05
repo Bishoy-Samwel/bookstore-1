af2cc5e776bbbe903c97497f90ab5aebc5c19b3b
Bishoy <bishoy.samwuel@gmail.com>
2021-11-05 20:08:01 +0200

    Using composeWithDevTools, add isLoading reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,18 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import booksReducer from './books/books';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { booksReducer, isLoading } from './books/books';
 
 const reducer = combineReducers({
   books: booksReducer,
+  isLoading,
   // additional reducers could be added here
 });
 
 const store = createStore(
-  reducer, applyMiddleware(thunk, logger),
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk, logger)),
 );
 
 export default store;
