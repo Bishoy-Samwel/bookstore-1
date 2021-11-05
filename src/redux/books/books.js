1caf813fb19046c387ab8836e603bf5a76de584e
Bishoy <bishoy.samwuel@gmail.com>
2021-11-05 17:35:56 +0200

    Edit the book reducer file: 1- Add 3 action types and their action creators: loadBooksInProgress, loadBooksSuccess, loadBooksFailure, 2-  Add isLoading reducer to track progress

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,20 +1,40 @@
 // Actions Types
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const LOAD_BOOKS_IN_PROGRESS = 'LOAD_BOOKS_IN_PROGRESS';
+const LOAD_BOOKS_SUCCESS = 'LOAD_BOOKS_SUCCESS';
+const LOAD_BOOKS_FAILURE = 'LOAD_BOOKS_FAILURE';
 
-// Reducer
+// Reducers
 const initialState = [];
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
+export const booksReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
     case ADD_BOOK:
-      return [...state, action.payload];
+      return [...state, payload];
     case REMOVE_BOOK:
-      return state.filter(book => book.id !== action.payload);
+      return state.filter(book => book.id !== payload);
+    case LOAD_BOOKS_SUCCESS: {
+      const { books } = payload;
+      return books;
+    }
     default:
       return state;
   }
 };
-export default reducer;
+
+export const isLoading = (state = false, action) => {
+  const { type } = action;
+
+  switch (type) {
+    case LOAD_BOOKS_IN_PROGRESS:
+      return true;
+    case LOAD_BOOKS_FAILURE:
+      return false;
+    default:
+      return state;
+  }
+};
 
 // Action Creators
 export const addBook = payload => ({
@@ -26,6 +46,18 @@ export const removeBook = payload => ({
   payload,
 });
 
+export const loadBooksInProgress = () => ({
+  type: LOAD_BOOKS_IN_PROGRESS,
+});
+
+export const loadBooksSuccess = books => ({
+  type: LOAD_BOOKS_SUCCESS,
+  payload: { books },
+});
+
+export const loadBooksFailure = () => ({
+  type: LOAD_BOOKS_FAILURE,
+});
 // side effects, only as applicable
 // e.g. thunks, epics, etc
 export const effect = () => {
